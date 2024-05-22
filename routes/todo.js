2993b53bf7d40c142e6e713cffda993de9c35924
Sentiments <qianqian1903@gmail.com>
2024-05-22 22:18:11 +0800

    added single todo to route

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createTodo, deleteTodo, getTodos } from "../controllers/todo.js";
+import { createTodo, deleteTodo, getTodo, getTodos } from "../controllers/todo.js";
 
 const router = express.Router();
 
 router.route("/").get(getTodos).post(createTodo);
-router.route("/:id").delete(deleteTodo);
+router.route("/:id").delete(deleteTodo).get(getTodo);
 
 export default router;
